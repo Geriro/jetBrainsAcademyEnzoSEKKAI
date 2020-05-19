306d3d8049675618fec8993723a4f6d28d43b13e
Enzo SEKKAI <gtegto@yahoo.fr>
2020-05-19 14:47:58 +0200

    adding problems

--- Problems/Groundhogs at a party/src/Main.java
+++ Problems/Groundhogs at a party/src/Main.java
@@ -4,6 +4,8 @@ class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int num = scanner.nextInt();
-        System.out.println(num < 10 && num > 0);
+        boolean weekend = scanner.nextBoolean();
+
+        System.out.println(num >= 10 && num <= 20 && !weekend || num >= 15 && num <= 25 && weekend);
     }
 }
